f54ba90f58810937c6e840e5bf53b6f26a39d15a
Olga Shonova <shonova.shonova@gmail.com>
2023-05-17 17:26:59 +0200

    added s7_count.py file

---
+++ 7_collections_and_data_manipulation/s7_count.py
@@ -0,0 +1,4 @@
+''' S7: Count the occurrences of the word 'ReDI' in the phrase: “ReDI school is awesome! Yes ReDI is really cool".'''
+
+phrase = "ReDI school is awesome! Yes ReDI is really cool"
+print("Number of occurrence of 'ReDI':", phrase.count("ReDI"))
